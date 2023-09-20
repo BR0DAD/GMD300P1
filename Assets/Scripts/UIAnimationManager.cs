2b109ccada7022808e1e68bab9df5d1cca681d59
MTL\bwenzel <brodywenzel89@gmail.com>
2023-09-20 16:58:09 -0400

    Final class changes

---
+++ Assets/Scripts/UIAnimationManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class UIAnimationManager : MonoBehaviour
+{
+    public static UIAnimationManager instance;
+
+    private Animator animator;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;  
+
+        } else if(instance != this)
+        {
+            Destroy(this);
+        }
+
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnDisable()
+    {
+        if (instance == this ) 
+        {
+            instance = null;
+        }
+    }
+
+    public void ShowInteractPrompt(bool showPrompt)
+    {
+        animator.SetBool("showInteractionPrompt", showPrompt);
+    }
+
+}
